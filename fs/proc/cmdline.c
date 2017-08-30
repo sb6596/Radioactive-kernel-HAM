3ce7147051c512a1a598d1cd7faeda1091b2150b
sb6596 <shubhamprince111@gmail.com>
2017-08-30 14:12:21 +0000

    selinux: make it permissive

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -36,22 +36,6 @@ static const struct file_operations cmdline_proc_fops = {
 
 static int __init proc_cmdline_init(void)
 {
-    /* SafetyNet bypass: show androidboot.verifiedbootstate=green */
-  	char *a1, *a2;
-  
-  	a1 = strstr(saved_command_line, "androidboot.verifiedbootstate=");
-  	if (a1) {
-  		a1 = strchr(a1, '=');
-  		a2 = strchr(a1, ' ');
-  		if (!a2) /* last argument on the cmdline */
-  			a2 = "";
-  
-  		scnprintf(proc_cmdline, COMMAND_LINE_SIZE, "%.*sgreen%s",
-  			  (int)(a1 - saved_command_line + 1),
-  			  saved_command_line, a2);
-  	} else {
-  		strncpy(proc_cmdline, saved_command_line, COMMAND_LINE_SIZE);
-  	}
 #ifdef CONFIG_SECURITY_SELINUX_PERMISSIVE
 	char *a1, *a2;
 
